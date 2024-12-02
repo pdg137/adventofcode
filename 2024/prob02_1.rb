8eadb85486feb356c71ab195eaba0f6a0a09987a
Paul Grayson <pdg@alum.mit.edu>
2024-12-01 23:02:43 -0800

    refactor

--- 2024/prob02_1.rb
+++ 2024/prob02_1.rb
@@ -1,23 +1,15 @@
-score = 0
+module Enumerable
+  def safe?
+    diffs = (length-1).times.collect { |i|
+      self[i+1] - self[i]
+    }
 
-def safe?(a)
-  increasing = (a.length-1).times.all? do |i|
-    diff = a[i+1] - a[i]
-    [1,2,3].index(diff)
+    diffs.all?(1..3) || diffs.all?(-3..-1)
   end
-
-  decreasing = (a.length-1).times.all? do |i|
-    diff = a[i+1] - a[i]
-    [-3,-2,-1].index(diff)
-  end
-
-  increasing || decreasing
 end
 
-File.open('data02.txt').each do |line|
-  a = line.split.map(&:to_i)
-
-  score += 1 if safe?(a)
+score = File.open('data02.txt').count do |line|
+  line.split.map(&:to_i).safe?
 end
 
 puts score
