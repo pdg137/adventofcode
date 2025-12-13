29bc3213c0b911c53b97c20495f410f478a8121b
Paul Grayson <pdg@alum.mit.edu>
2025-12-13 10:13:19 -0800

    working propagation

--- 2025/prob07_1.sql
+++ 2025/prob07_1.sql
@@ -1,25 +1,48 @@
-CREATE TABLE "lines" (v TEXT, n INTEGER PRIMARY KEY);
-.import data07.txt lines
-SELECT * FROM lines;
+CREATE TABLE "input" (v TEXT, n INTEGER PRIMARY KEY);
+.import data07.txt input
+SELECT * FROM input;
 
 
-CREATE TABLE vars (name TEXT, value INTEGER);
-insert into vars values ('rows', (select count(*) from lines));
-insert into vars values ('cols', (select length(v) from lines where n=1));
+CREATE TABLE vars (rows INTEGER, cols INTEGER);
+INSERT INTO vars VALUES (
+  (select count(*) from input),
+  (select length(v) from input where n=1)
+  );
 
-WITH RECURSIVE data(x, y, v) as (
-     select 1, 1, (select substring(v, 1, 1) from lines where n=1)
-     union all
-     select
-        x % (select value from vars where name='cols') + 1,
-        y + (x = (select value from vars where name='cols')),
-        (select substring(
-                v,
-                x % (select value from vars where name='cols') + 1,
-                1
-        ) from lines where n = y + (x = (select value from vars where name='cols'))
+WITH RECURSIVE output(prev, cur, n, x) as (
+  SELECT v, '', 1, 0 FROM input WHERE n=1
+  UNION ALL
+  -- input.v is the current row of the input data
+  -- output.prev is the previous row of the output data
+  -- output.cur is the current row of the output data, built up to char x-1
+  --
+  SELECT
+    if(x = cols+1, output.cur, output.prev),
+    if(x = cols+1, '',
+      concat(if(x = cols+1, '',output.cur),
+        -- compute the next character of the current line
+        IF(
+          -- case 1
+          'S' is SUBSTR(output.prev, x, 1),
+          '|',
+
+          -- case 2
+          ('^' is SUBSTR(input.v, x-1, 1)) OR ('^' is SUBSTR(input.v, x+1, 1)),
+          '|',
+
+          -- case 3
+          ('|' is SUBSTR(output.prev, x, 1)) AND ('.' is SUBSTR(input.v, x, 1)),
+          '|',
+
+          -- else
+          SUBSTR(input.v, x, 1)
+          )
         )
-        from data
-     limit (select value from vars where name='cols')*(select value from vars where name='rows')
+      ),
+    if(x = cols+1, output.n+1, output.n),
+    if(x = cols+1, 1, x+1) -- x = x+1
+    FROM input, output, vars
+    WHERE input.n = output.n+1
+    LIMIT (SELECT rows*cols FROM vars)
 )
-select * from data;
+SELECT * FROM output WHERE x = (SELECT cols from vars) LIMIT 20;
